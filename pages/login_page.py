891e2ca03ee59cf5a54826f5153c72518565800b
Deepak Gadhave <91776078+Deepakgadhave9527@users.noreply.github.com>
2026-04-01 11:27:12 +0530

    Added Selenium automation scripts

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,19 +1,16 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 class LoginPage:
-    LOGIN_PAGE_URL = "https://practicetestautomation.com/practice-test-login"
+    LOGIN_URL = "https://practicetestautomation.com/practice-test-login"
     USERNAME_FIELD = (By.CSS_SELECTOR, "#username")
     PASSWORD_FIELD = (By.CSS_SELECTOR, "#password")
     LOGIN_BUTTON = (By.CSS_SELECTOR, "#submit")
-    DASHBOARD_URL = "https://practicetestautomation.com/logged-in-successfully"
 
     def __init__(self, driver):
         self.driver = driver
 
-    def open_login_page(self):
-        self.driver.get(self.LOGIN_PAGE_URL)
+    def navigate_to_login_page(self):
+        self.driver.get(self.LOGIN_URL)
 
     def enter_username(self, username):
         self.driver.find_element(*self.USERNAME_FIELD).send_keys(username)
@@ -23,6 +20,3 @@ class LoginPage:
 
     def click_login_button(self):
         self.driver.find_element(*self.LOGIN_BUTTON).click()
-
-    def verify_dashboard_page_loaded(self):
-        WebDriverWait(self.driver, 10).until(EC.url_to_be(self.DASHBOARD_URL))
