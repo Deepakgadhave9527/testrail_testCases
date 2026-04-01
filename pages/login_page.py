11f786a8376a80c052bbcf3b1cbadc857ea9739d
Deepak Gadhave <91776078+Deepakgadhave9527@users.noreply.github.com>
2026-04-01 11:18:12 +0530

    Added Selenium automation scripts

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,21 +3,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class LoginPage:
+    LOGIN_PAGE_URL = "https://practicetestautomation.com/practice-test-login"
+    USERNAME_FIELD = (By.CSS_SELECTOR, "#username")
+    PASSWORD_FIELD = (By.CSS_SELECTOR, "#password")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "#submit")
+    DASHBOARD_URL = "https://practicetestautomation.com/logged-in-successfully"
+
     def __init__(self, driver):
         self.driver = driver
-        self.url = "https://practicetestautomation.com/practice-test-login"
-        self.username_field = (By.CSS_SELECTOR, "#username")
-        self.password_field = (By.CSS_SELECTOR, "#password")
-        self.login_button = (By.CSS_SELECTOR, "#submit")
 
-    def open(self):
-        self.driver.get(self.url)
+    def open_login_page(self):
+        self.driver.get(self.LOGIN_PAGE_URL)
 
     def enter_username(self, username):
-        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.username_field)).send_keys(username)
+        self.driver.find_element(*self.USERNAME_FIELD).send_keys(username)
 
     def enter_password(self, password):
-        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.password_field)).send_keys(password)
+        self.driver.find_element(*self.PASSWORD_FIELD).send_keys(password)
 
-    def click_login(self):
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.login_button)).click()
+    def click_login_button(self):
+        self.driver.find_element(*self.LOGIN_BUTTON).click()
+
+    def verify_dashboard_page_loaded(self):
+        WebDriverWait(self.driver, 10).until(EC.url_to_be(self.DASHBOARD_URL))
