5146a41d2ecbd335ba716ea0dd1c78a679cbe9f6
Deepak Gadhave <91776078+Deepakgadhave9527@users.noreply.github.com>
2026-04-01 11:18:12 +0530

    Added Selenium automation scripts

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,13 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.webdriver import WebDriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-@pytest.fixture(scope="function")
-def setup():
-    driver = webdriver.Chrome()
+@pytest.fixture(scope='function')
+def driver():
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
     driver.maximize_window()
     yield driver
     driver.quit()
